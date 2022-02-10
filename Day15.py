d560eea216ce65d76339cb37f0c58cb3bae55c21
Rahul Gupta <59315082+iamrahul-9@users.noreply.github.com>
2022-02-10 22:08:01 +0530

    Add files via upload

--- Day15.py
+++ Day15.py
@@ -1,39 +1,32 @@
-def merge(arr,left,mid,right):
-    n1 = mid - left + 1
-    n2 = right - mid
-    a= [n1]
-    b = [n2]
-    for i in range(n1):
-        a[i] = arr[left + i]
-    for i in range(n2):
-        b[i] = arr[mid + 1 + i]
-    i = 0
-    j = 0
-    k = left
-    while i < n1 and j < n2:
-        if a[i] < b[j]:
+def merge_sort(arr):
+    # base condition
+    if len(arr) > 1:
+        left_arr = arr[:len(arr)//2]
+        right_arr = arr[len(arr)//2:]
+        # recursion
+        merge_sort(left_arr)
+        merge_sort(right_arr)
+        # merge
+        i = j = k = 0
+        while i < len(left_arr) and j < len(right_arr):
+            if left_arr[i] < right_arr[j]:
+                arr[k] = left_arr[i]
+                i += 1
+            else:
+                arr[k] = right_arr[j]
+                j += 1
             k += 1
-            j += 1
-        else:
-            arr[k] = b[j]
-            k += 1
+
+        while i < len(left_arr):
+            arr[k] = left_arr[i]
             i += 1
-    while i < n1:
-        arr[k] = a[i]
-        k += 1
-        i += 1
-    while i < n2:
-        arr[k] = b[j]
-        k += 1
-        j += 1
+            k += 1
 
-def merge_sort(arr,left,right):
-    if left < right:
-        mid = (left + right)/2
-        merge_sort(arr,left,mid)
-        merge_sort(arr,mid+1,right)
-        merge(arr,left,mid,right)
+        while j < len(right_arr):
+            arr[k] = right_arr[j]
+            j += 1
+            k += 1
 
-arr = [5,4,3,2,1]
-merge_sort(arr,0,4)
+arr = [5,8,95,30,1,0,4,3,2]
+merge_sort(arr)
 print(arr)
